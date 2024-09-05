36d44a67315b44318ad393a7bfbdc1e99f9fe054
Kongtup <ttansiri524@gmail.com>
2024-09-05 17:43:49 +0700

    init parkplus

---
+++ template/index.php
@@ -0,0 +1,10 @@
+<?php $root = ""; ?>
+
+<?php include($root . "header.php"); ?>
+
+<main id="main" class="index-main">
+    <h1>ENGLISH</h1>
+    <h1>ไทย</h1>
+</main>
+
+<?php include($root . "footer.php"); ?>
